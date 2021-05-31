dc698ff068df9cfa0f7b0b8f14804d8f0dd10f2e
Andy Chong Sam <achongs.business@gmail.com>
2021-05-31 15:08:17 -0400

    new RestAssured tests added.

--- src/test/java/com/finance/controller/MainControllerTest.java
+++ src/test/java/com/finance/controller/MainControllerTest.java
@@ -1,6 +1,8 @@
 package com.finance.controller;
 
-import io.restassured.response.Response;
+import com.finance.model.calculations.AmortEntry;
+
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,16 +27,31 @@ class MainControllerTest {
 
     @Test
     public void amortEndpointTest() {
-        Response response = get("api/amort?apy=7.3&loanAmount=73000&period=60")
-                .then().extract().response();
 
-        int periods = response.jsonPath().getList("$").size();
-        assertEquals(60, periods);
+        AmortEntry  entries [] = get("api/amort?apy=0.073&loanAmount=73000&period=60")
+                .as(AmortEntry[].class);
+
+        AmortEntry firstEntry = entries[0];
+        assertEquals("$73000.00",firstEntry.getBegBalance().trim() );
+
+        AmortEntry lastEntry = entries[entries.length-1];
+        assertEquals("$0.00", lastEntry.getEndBalance().trim());
+
     }
 
 
     @Test
     public void npvEndpointTest() {
+
+        String [] cashFlowArr = {"100.00", "200.00", "300.00"};
+        String testRate = "11";
+        JSONObject body = new JSONObject();
+        body.put("cashFlows", cashFlowArr);
+        body.put("rate", testRate);
+
+        given().contentType("application/json")
+        .body(body.toString()).when().post("/api/npv").then().statusCode(200)
+                .assertThat().body("npv", equalTo(471.77f));
 
     }
 }
